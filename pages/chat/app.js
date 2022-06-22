39c59b859bea75ee6691b4e773891b320fcaf09a
hoa.nguyenduc <nguyenduchoa0130@gmail.com>
2022-06-22 15:03:01 +0700

    add search

--- pages/chat/app.js
+++ pages/chat/app.js
@@ -7,6 +7,8 @@ const groupChat = document.getElementById('groupChat');
 const startChat = document.getElementById('startChat');
 const chatPersonalDetail = document.getElementById('chat__personal__detail');
 const chatGroupDetail = document.getElementById('chat__group__detail');
+const findMember = document.getElementById('findMember');
+const findMemberIcon = document.getElementById('findMemberIcon');
 //#endregion
 
 //#region Utils
@@ -70,11 +72,10 @@ groupLink.addEventListener('click', function () {
 
 
 //#region Handle Click chat item
-
 const chatPersonItems = document.querySelectorAll('#personalChat .list-group-item');
 chatPersonItems.forEach((item) => {
   item.addEventListener('click', function () {
-    handleItemChatClick(this, [chatGroupDetail, startChat], [chatPersonalDetail], 'list-group-item--active')();
+    handleItemChatClick(this, [chatGroupDetail, startChat, findMember], [chatPersonalDetail], 'list-group-item--active')();
     setActiveUser(this, '.chat__item__user__name', 'chat__personal__detail__username', '.chat__item__avatar img', 'chat__personal__detail__avatar');
   });
 });
@@ -82,7 +83,17 @@ chatPersonItems.forEach((item) => {
 const chatGroupItems = document.querySelectorAll('#groupChat .list-group-item');
 chatGroupItems.forEach((item) => {
   item.addEventListener('click', function () {
-    handleItemChatClick(this, [chatPersonalDetail, startChat], [chatGroupDetail], 'list-group-item--active')();
+    handleItemChatClick(this, [chatPersonalDetail, startChat, findMember], [chatGroupDetail], 'list-group-item--active')();
     setActiveUser(this, '.chat__item__user__name', 'chat__group__detail__username', '.chat__item__avatar img', 'chat__group__detail__avatar');
   });
 });
+//#endregion
+
+
+//#region Handle Find Member To Chat
+findMemberIcon.addEventListener('click', function () {
+  hideElement([startChat, chatGroupDetail, chatPersonalDetail]);
+  showElement([findMember]);
+  removeActive('list-group-item--active');
+});
+
